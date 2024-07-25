36580e39e347182705cae233d361588442bc081d
Amrutha <amruthaur99@gmail.com>
2024-07-25 11:19:25 +0400

    design change

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,7 @@ Route::middleware(['auth'])->group(function () {
     Route::middleware(['user'])->group(function () {
         Route::resource('/ticket', TicketController::class)->except(['destroy']);
         Route::get('/ticketlisting', [TicketController::class, 'ticketlisting'])->name('ticketlist');
+        Route::get('/reply/{ticketid}', [TicketController::class, 'replyticket'])->name('reply');
     });
 
     // Ensure /ticket and /ticket/create are accessible to both roles
