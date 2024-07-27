b8131ff92fa5b8f1b6b8e44a24c361e31b8c14fd
deepika <deepika.g@crystawall.com>
2024-07-27 11:30:40 +0400

    Contact us,Sanitisation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->count()->create();
+        User::factory()->count(10)->create();
 
         Ticket::factory()->count(20)->create();
 
