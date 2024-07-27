33f119b5aa94bfba92ca760a618e8527b14a9a4d
Amrutha <amruthaur99@gmail.com>
2024-07-27 10:06:42 +0400

    Contact

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+ 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\TicketController;
@@ -15,20 +15,20 @@ use App\Http\Controllers\CategoryController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
+ 
+ 
 Auth::routes();
 Route::controller(GithubController::class)->group(function(){
     Route::get('auth/github', 'redirectToGithub')->name('auth.github');
     Route::get('auth/github/callback', 'handleGithubCallback');
 });
-
+ 
 // GoogleLoginController redirect and callback urls
 Route::controller(GoogleLoginController::class)->group(function(){
     Route::get('auth/google', 'redirectToGoogle')->name('auth.google');
     Route::get('auth/google/callback', 'handleGoogleCallback');
 });
-
+ 
 Route::get('/admin',function(){
     return view('admin-theme.dashboard');
 });
@@ -37,46 +37,48 @@ Route::get('/blogs', [HomeController::class, 'blogs'])->name('blogs');
 Route::get('/productlist', [HomeController::class, 'products'])->name('productlist');
 
 
+Route::get('/contact-us', [HomeController::class, 'contactus'])->name('contact-us');
+Route::post('/add-contact',[HomeController::class,'addContact'])->name('addcontact');
 Route::middleware(['auth'])->group(function () {
-
+ 
     Route::middleware(['admin'])->group(function () {
         Route::resource('/ticket', TicketController::class);
         Route::post('/ticket/{id}/toggle-status', [TicketController::class, 'toggleStatus'])->name('ticket.toggleStatus');
         Route::get('/reply/{ticketid}', [TicketController::class, 'replyticket'])->name('reply');
         Route::post('/replyupdate', [TicketController::class, 'replyupdate'])->name('replyupdate');
         Route::get('/ticketlisting', [TicketController::class, 'ticketlisting'])->name('ticketlist');
-
+ 
         Route::resource('/users', UserController::class);
         Route::get('/users/delete/{id}', [UserController::class, 'delete'])->name('delete');
-
+ 
         Route::resource('/posts', 'App\Http\Controllers\PostController');
         Route::get('posts/{id}/delete','App\Http\Controllers\PostController@destroy');
-
+ 
         Route::get('/posts/{post}/publish','App\Http\Controllers\PostController@publish');
-        
+       
         Route::resource('/category', CategoryController::class);
         Route::get('/category/delete/{id}', [CategoryController::class, 'delete'])->name('delete');
-
+ 
         Route::resource('/products', ProductController::class);
         Route::get('/products/delete/{id}', [ProductController::class, 'delete'])->name('delete');
-        
-
+       
+ 
     });
-
+ 
     Route::middleware(['user'])->group(function () {
         Route::resource('/ticket', TicketController::class)->except(['destroy']);
         Route::get('/ticketlisting', [TicketController::class, 'ticketlisting'])->name('ticketlist');
-        
+       
     });
     Route::get('/reply/{ticketid}', [TicketController::class, 'replyticket'])->name('reply');
     // Ensure /ticket and /ticket/create are accessible to both roles
     Route::resource('/ticket', TicketController::class);
     Route::get('/blog', [App\Http\Controllers\BlogController::class, 'index'])->name('blog_list');
 // Route::get('/blog/{id}', [App\Http\Controllers\BlogController::class, 'post'])->name('blogs');
-    
+   
 });
-
-
+ 
+ 
 // Route::get('/auth/redirect', function () {
 //     return Socialite::driver('github')
 //     ->scopes(['read:user', 'public_repo'])
@@ -99,3 +101,4 @@ Route::middleware(['auth'])->group(function () {
 //  dd($user);
 //     return redirect('/home');
 // });
+ 
