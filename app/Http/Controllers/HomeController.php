3f6b80549282ecfb8bffdf6742fecfe320387485
Amrutha <amruthaur99@gmail.com>
2024-07-27 15:17:37 +0400

    tinymce,admin dashboard,product

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\Product;
 use App\Models\Category;
+use App\Models\Post;
 
 use App\Mail\SendContactEmail;
 use App\Models\Contact;
@@ -29,10 +30,11 @@ class HomeController extends Controller
     public function index()
     {
       
-        $data['home_products'] = Product::with('category')->get();
+        $data['home_products']      = Product::with('category')->get();
         $data['home_category']      = Category::get();
+        $data['home_blogs']         = Post::orderBy('id', 'desc')->limit(6)->get();
         $data['organic_vegetables'] = Product::with('category')->where('category_id','5')->get();
-        $data['organic_fruits'] = Product::with('category')->where('category_id','4')->get();
+        $data['organic_fruits']     = Product::with('category')->where('category_id','4')->get();
         
         return view('frontend/index',$data);
     }
@@ -42,8 +44,27 @@ class HomeController extends Controller
     }
     public function products()
     {
-        $data['categorys']      = Category::get();
+        $data['categorys']      = Category::with('product')->get();
         $data['products']       = Product::with('category')->get();
+        return view('frontend/product',$data);
+    }
+    public function products_details($id){
+
+        $data['category']           = Category::with('product')->get();
+        $data['product'] = $product = Product::with('category')->find($id);
+        // Get the category of the product
+        $category = $product->category;
+        $data['relatedProducts'] = $category->product()->where('id', '!=', $product->id)->get();
+        return view('frontend/product_detail',$data);
+    }
+    public function products_with_category($id)
+    {
+
+        // $data['category'] = Category::with('product')->where('id',$id)->get();
+        $data['products'] =Product::with('category')->whereHas('category', function($query)use($id) {$query->where('id', $id); }) ->get();
+
+        // dd($data['products']->title);
+        $data['categorys']      = Category::get();
         return view('frontend/product',$data);
     }
 
