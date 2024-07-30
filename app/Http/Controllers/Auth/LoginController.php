ac8c9ecb1e190c63194b11f48f15084bed1bc888
deepika <deepika.g@crystawall.com>
2024-07-30 17:06:57 +0400

    authentication redirections ,laravel dusk

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -25,7 +27,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/dashboard';
+  //  protected $redirectTo = '/dashboard';
 
     /**
      * Create a new controller instance.
@@ -36,5 +38,17 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
         $this->middleware('auth')->only('logout');
+    }
+
+    protected function authenticated(Request $request, $user)
+    {
+        // Determine the redirect URL based on user attributes
+        if ($user->role_id == 1) {
+            // Redirect admins to the admin dashboard
+            return redirect('/dashboard');
+        }  else {
+            // Redirect regular users to the default dashboard
+            return redirect('/home');
+        }
     }
 }
